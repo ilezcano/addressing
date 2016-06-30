c999d99ec1e4f165392329d624de179f5bbb6461
Ismael Lezcano <ish@ishlaptop.lezcano.org>
2016-06-30 15:04:57 -0400

    Removed the BEFORE output of the cidragg script. Not really necessary

--- cidragg.pl
+++ cidragg.pl
@@ -7,15 +7,11 @@ use strict;
 my @smartcidr;
 $\ = $, = "\n";
 
-print '=' x 25 . 'BEFORE' . '=' x 25;
 while (<>)
 	{
 	my $cidr = cidrvalidate($_);
 	next unless $cidr;
-	print $cidr;
 	@smartcidr = Net::CIDR::cidradd($cidr, @smartcidr);
 	}
-
-print '=' x 25 . 'AFTER' . '=' x 25;
 
 print @smartcidr;
