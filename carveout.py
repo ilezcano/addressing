002ba14fc28588f7294fd51bfa4ea1a909ea8b53
Ismael <ismael.lezcano@gmail.com>
2019-10-05 18:05:11 -0400

    Streamlined carveout

--- carveout.py
+++ carveout.py
@@ -12,11 +12,8 @@ infile = arguments.parse_known_args()[0].infile
 
 prefix = netaddr.IPNetwork(prefixarg)
 
-cidrlist = list()
 for line in infile.readlines():
     try: cidr = netaddr.IPNetwork(line)
     except netaddr.core.AddrFormatError: continue
-    cidrlist += netaddr.cidr_exclude(cidr, prefix)
+    for a in  netaddr.cidr_exclude(cidr, prefix): print(a)
         
-for a in cidrlist:
-    print(a)
